d28865d3034abda662007a3a49eebff176f7328e
shohei-saito07 <56618646+shohei-saito07@users.noreply.github.com>
2023-12-10 09:05:00 +0900

    PHP基礎の課題07

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>課題07</title>
+ </head>
+ 
+ <body>
+     <p>
+         <?php
+         // 連想配列を作成する
+         $food_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+         // 連想配列列を出力する
+         print_r($food_data);
+         ?>
+     </p>
+ </body>
+ 
+ </html>
